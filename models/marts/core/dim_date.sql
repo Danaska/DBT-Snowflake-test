fa59a3f172f0414eb1f1c45c51a78f78856aa002
Danaska Alonso Martinez <danaska.alonso@gmail.com>
2024-06-12 12:14:40 +0000

    tests and docs

--- models/marts/core/dim_date.sql
+++ models/marts/core/dim_date.sql
@@ -1,15 +1,5 @@
 with source as (
     select * from {{ ref('stg_demo_date') }}
-),
-
-selected as (
-    select
-        Date_Id,
-        date,
-        Year,
-        Month,
-        Quarter
-    from source
 )
 
-select * from selected
+select * from source
